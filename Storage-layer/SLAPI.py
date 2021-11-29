9799efd7b1fce74f5cf0b4c19f31bb3c591d1f7f
marteinn_vidir <marteinns19@ru.is>
2021-11-29 19:40:37 +0000

    aaa

---
+++ Storage-layer/SLAPI.py
@@ -0,0 +1,36 @@
+
+
+class SlAPI:
+    def __init__(self):
+        self.empDL = None # þarf að bæta við
+        self.locDL = None # þarf að bæta við
+        self.houseDL = None #þarf að bæta við
+        self.contDL = None #þarf að bæta við
+        self.reportDL = None #þarf að bæta við
+
+
+    def get_all_emp(self):
+        return self.empDL # þarf að klára
+
+    def add_emp(self,emp_lis):
+        self.empDL # þarf að klára
+
+    def change_emp_info(self):
+        pass
+
+    def get_loc_info(self):
+        pass
+
+    def get_house_info(self):
+        pass
+
+    def 
+
+
+
+
+        
+
+
+if __name__ == "__main__":
+
