8f2c03b33ca8369bf1e0917d98a9e2c59550e8ef
Lsig <logis21@ru.is>
2021-11-29 16:46:35 +0000

    emp menu update

--- UI/emp_main_menu.py
+++ UI/emp_main_menu.py
@@ -1,1 +1,34 @@
-#UIhönnun
+#Employee Main Menu 
+STAR = '* '
+DASH = '-'
+class Employee_Main: 
+    def __init__(self):
+        self.options = f''' Location| Name | Staff ID 
+{STAR*14}
+      {DASH*15}
+      1. Fasteignir 
+      2. Viðhald
+      3. Verktakar
+      {DASH*15}
+{STAR*14}
+        '''
+
+    def print_menu(self):
+        print(self.options)
+        self.user_input()
+
+    def user_input(self):
+        while True:   
+            user_choice = input()
+            if user_choice == '1':
+                prop_menu = ''
+            elif user_choice == '2':
+                maintnence_menu = ''
+            elif user_choice == '3':
+                contract_menu = ''
+            else:
+                print('Invalid choice, try again!')
+        
+
+menu = Employee_Main()
+menu.print_menu()
