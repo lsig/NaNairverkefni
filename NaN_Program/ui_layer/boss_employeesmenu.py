564245a38ef96e43c5c4c6c7af8e0618bf747765
Lsig <logis21@ru.is>
2021-11-30 14:59:48 +0000

    empbmenu

--- NaN_Program/ui_layer/boss_employeesmenu.py
+++ NaN_Program/ui_layer/boss_employeesmenu.py
@@ -1,1 +1,33 @@
 #starfsmannagluggi
+#Employee Main Menu 
+STAR = '* '
+DASH = '-'
+class BossEmployeesMenu: 
+    def __init__(self, id):
+        self.id = id
+        self.options = f''' 
+
+ Location | Name | {self.id} 
+{STAR*14}
+      {DASH*15}
+      1. Skrá nýjan starfsmann
+      2. Starfsmannalisti
+      {DASH*15}
+      B. Til baka
+{STAR*14}
+        '''
+
+    def display(self):
+        print(self.options)
+    
+    def user_input(self):
+        while True:   
+            user_choice = input()
+            if user_choice == '1':
+                new_emp = ''
+            elif user_choice == '2':
+                emp_list = ''
+            elif user_choice == 'B':
+                return
+            else:
+                print('Invalid choice, try again!')
