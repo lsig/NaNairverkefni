477beb6c5e5cf39925e2518ed9fc22dc1d4dce2b
marteinn_vidir <marteinns19@ru.is>
2021-11-30 13:52:15 +0000

    bla

---
+++ NaN_Program/Storage-layer/DLAPI.py
@@ -0,0 +1,84 @@
+
+
+class DlAPI:
+    def __init__(self):
+        self.empDL = None # þarf að bæta við
+        self.locDL = None # þarf að bæta við
+        self.houseDL = None #þarf að bæta við
+        self.contDL = None #þarf að bæta við
+        self.reportDL = None #þarf að bæta við
+        self.jobDL = None # þarf að bæta við
+        self.maintenjobDL = None # þarf að bæta við
+
+
+    def get_all_emp(self):
+        return self.empDL # þarf að klára
+
+    def add_emp(self,emp_lis):
+        self.empDL # þarf að klára
+
+    def change_emp_info(self,all_emp_lis):
+        self.empDL # þarf að klára
+
+    def get_loc_info(self):
+        return self.locDL # þarf að klára
+
+    def add_loc(self,loc_lis):
+        self.locDL # þarf að klára
+
+    def get_house_info(self):
+        return self.houseDL # þarf að klára
+
+    def add_house(self,house_lis):
+        self.houseDL # þarf að klára
+
+    def change_house_info(self,all_house_lis):
+        self.houseDL #þarf að klára
+
+    def get_all_cont(self):
+        return self.contDL # þarf að klára
+
+    def add_cont(self,con_lis):
+        self.contDL # þarf að klára
+
+    def change_cont(self,all_con_lis):
+        self.contDL # þarf að klára
+
+    def get_all_report(self):
+        return self.reportDL # þarf að klára
+
+    def add_report(self,report):
+        self.reportDL # þarf að klára
+
+    def change_report(self,reports):
+        self.reportDL # þarf að klára
+
+    def get_jobs(self):
+        return self.jobDL # þarf að klára 
+
+    def add_job(self,job):
+        self.jobDL # þarf að klára
+
+    def change_job(self,jobs):
+        self.jobDL # þarf að klára
+
+    def get_maintenance_jobs(self):
+        return self.maintenjobDL # þarf að klára
+
+    def add_maintenance_job(self,main_job):
+        self.maintenjobDL # þarf að klára
+
+    def change_maintenance_job(self,main_jobs):
+        self.maintenjobDL # þarf að klára
+
+
+    
+
+
+
+
+        
+
+
+if __name__ == "__main__":
+
