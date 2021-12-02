a86a85ddb60bd2ec288937e8fb9e79d76620846f
Maximpop <rudkovpop@gmail.com>
2021-12-02 15:47:22 +0000

    JobDL

---
+++ NaN_Program/storage_layer/job.py
@@ -0,0 +1,19 @@
+from employee import Employee
+
+
+class Job:
+    def __init__(self,id,date_created,employee,employee_id,description ,location,propertyName,propertyNumber,propertyid,priority,status):
+        self.id = id 
+        self.date_cr = date_created
+        self.empl = employee
+        self.empl_id = employee_id 
+        self.desc = description
+        self.loc = location
+        self.prop = propertyName
+        self.prop_num = propertyNumber
+        self.prop_id = propertyid
+        self.prior = priority
+        self.stat = status
+
+
+
