1512daecb6c2dea5428d305d4580194c5f97eaab
Maximpop <rudkovpop@gmail.com>
2021-12-02 17:30:11 +0000

    job fixed
    Title and Suggested-contractors added

--- NaN_Program/storage_layer/job.py
+++ NaN_Program/storage_layer/job.py
@@ -1,16 +1,20 @@
 class Job:
-    def __init__(self,id,date_created,employee,employee_id,description ,location,propertyName,propertyNumber,propertyid,priority,status):
+    def __init__(self,id,date_created,employee,employee_id,title,description ,location,propertyName,propertyNumber,propertyid,priority,suggested_contractors,status):
         self.id = id 
         self.date_cr = date_created
         self.empl = employee
         self.empl_id = employee_id 
+        self.titl = title
         self.desc = description
         self.loc = location
         self.prop = propertyName
         self.prop_num = propertyNumber
         self.prop_id = propertyid
         self.prior = priority
+        self.sug_con = suggested_contractors
         self.stat = status
+
+
 
 
 
--- NaN_Program/storage_layer/jobDL.py
+++ NaN_Program/storage_layer/jobDL.py
@@ -1,5 +1,5 @@
 import csv
-#from job import Job
+from job import Job
 
 class JobDL:
     def __init__(self):
@@ -19,9 +19,9 @@ class JobDL:
 
     def add_job(self,jobinfo):
         with open(self.csv, 'a', newline='', encoding='utf-8') as csvfile:
-            fieldnames = ["id","Date-created","Employee","Employee-id","Description","Location","Property","Property-number","Property-id","Priority","Status"]
+            fieldnames = ["id","Date-created","Employee","Employee-id","Title","Description","Location","Property","Property-number","Property-id","Priority","Suggested-contractors","Status"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({'id': jobinfo.id, "Date-created": jobinfo.date_cr, "Employee": jobinfo.empl, "Employee-id": jobinfo.empl_id, "Description": jobinfo.desc, "Location": jobinfo.loc,"Property":jobinfo.prop, "Property-number":jobinfo.prop_num,"Property-id":jobinfo.prop_id,"Priority":jobinfo.prior,"Status":jobinfo.stat})
+            writer.writerow({'id': jobinfo.id, "Date-created": jobinfo.date_cr, "Employee": jobinfo.empl, "Employee-id": jobinfo.empl_id,"Title":jobinfo.titl, "Description": jobinfo.desc, "Location": jobinfo.loc,"Property":jobinfo.prop, "Property-number":jobinfo.prop_num,"Property-id":jobinfo.prop_id,"Priority":jobinfo.prior,"Suggested-contractors":jobinfo.sug_con,"Status":jobinfo.stat})
 
 
     def change_job_info(self,job_lis):
@@ -30,17 +30,17 @@ class JobDL:
             header = job_lis[0]
             writer.writerow(header)
             for dic in job_lis:
-                writer.writerow([dic["id"],dic["Date-created"],dic["Employee"],dic["Employee-id"],dic["Description"],dic["Location"],dic["Property"],dic["Property-number"],dic["Property-id"],dic["Priority"],dic["Status"]])
+                writer.writerow([dic["id"],dic["Date-created"],dic["Employee"],dic["Employee-id"],dic["Title"],dic["Description"],dic["Location"],dic["Property"],dic["Property-number"],dic["Property-id"],dic["Priority"],dic["Suggested-contractors"],dic["Status"]])
         
             
 
 
 
 
-#if __name__ == "__main__":
-    #g = JobDL()
-    #lis = g.get_all_jobs()
-    #jobinfo = Job("1","0","John","1","Clean oven","Something","something2","1111","1","High","0")
+if __name__ == "__main__":
+    g = JobDL()
+    lis = g.get_all_jobs()
+    #jobinfo = Job("1","0","John","1","Clean!","Clean oven","Something","something2","1111","1","High","1","0")
     #g.add_job(jobinfo)
-    #g.change_job_info(lis)
+    g.change_job_info(lis)
         
