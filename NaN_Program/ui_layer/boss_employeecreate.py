bfb24453ead4e018f2b0189eb97e07020e13ba75
Lsig <logis21@ru.is>
2021-12-02 14:46:11 +0000

    updated employecreate

--- NaN_Program/ui_layer/boss_employeecreate.py
+++ NaN_Program/ui_layer/boss_employeecreate.py
@@ -1,35 +1,60 @@
 #skrá nýjan starfsmann
 #þarf að importa klösum eins og employee
-from data_files.const import STAR, DASH
 
-class BossEmployeeCreate: 
-    def __init__(self, id):
+
+from data_files.const import CLEAR, INVALID, STAR, DASH, SLEEPTIME
+from time import sleep
+import os
+CONTRACTTEMPLATE = ['Nafn', 'Kennitala', 'Heimilisfang', 'Heimasími', 'GSM símanúmer', 'Netfang', 'Áfangastaður', 'Yfirmaður']
+
+class BossEmployeeCreate:
+    def __init__(self, id) -> None:
         self.id = id
-        self.options = f''' 
+        self.contractlist = []
+        self.screen = f'''
  Location | Name | {self.id} 
 {STAR*14}
     | STARFSMENN |
-     - skrá nýjan starfsmann
+     - Skrá nýja starfsmann
       {DASH*15}
+    Q. Hætta við
         '''
     
     def display_menu(self):
-        print(self.options)
-        self.create_employee()
-        return 
+        os.system(CLEAR)
+        print(self.screen)
 
-    def create_employee(self):
-        name = input('Nafn starfsmanns: ')
-        social_sec = input('Kennitala starfsmanns: ')
-        address = input('Heimilisfang: ')
-        phone = input('Heimasími: ')
-        gsm =  input('GSM sími: ')
-        email = input('Netfang: ')
-        destination = input('Áfangastaður: ')
-        manager = input('Yfirmaður: ')
-        #new_employee = Employee(name,social_sec,address,phone,gsm,email,destination,manager)
-
-        if True: #TODO
-            return
+        for i in range(len(CONTRACTTEMPLATE)):
+            user_input = input(f"{CONTRACTTEMPLATE[i]}: ")
+            if user_input.upper() == 'Q':
+                return
+            self.contractlist.append(user_input)
+        
+        self.confirm_employee()
+        
+    
+    def make_employee(self):
+        contract_overview = '\n | STARFSMENN |\n'
+        listint = 0
+        for i in range(len(CONTRACTTEMPLATE)):
+            if self.contractlist[listint] != '':
+                contract_overview += f"{CONTRACTTEMPLATE[i]}: {self.contractlist[listint]}\n"
+                listint += 1
+        
+        return contract_overview
+    
+    def confirm_employee(self):
+        contract = self.make_employee()
+        print(contract)
+        confirm = input("""C. Confirm \nE. Edit \nQ. Quit \n""")
+        while True:
+            if confirm.upper() == 'C':  # TODO
+                return
+            elif confirm.upper() == 'E': # TODO
+                pass
+            elif confirm.upper() == 'Q': # TODO
+                return
+            else:
+                print(INVALID)
     
  
