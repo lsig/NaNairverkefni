e4916a05cd20ff1102922a417e07b3d5a5da7eab
marteinn_vidir <marteinns19@ru.is>
2021-12-02 17:10:53 +0000

    hda

--- NaN_Program/storage_layer/report.py
+++ NaN_Program/storage_layer/report.py
@@ -1,5 +1,5 @@
 class Report:
-    def __init__(self,report_id,request_id,emp,emp_id,title,descript,contract_name,contract_id,status):
+    def __init__(self,report_id,request_id,emp,emp_id,title,descript,contract_name,contract_id,contract_rating,status):
         self.report_id = report_id
         self.request_id = request_id
         self.emp = emp
@@ -8,6 +8,7 @@ class Report:
         self.descript = descript
         self.contract_name = contract_name
         self.contract_id = contract_id
+        self.contract_rating = contract_rating
         self.status = status
         
 
--- NaN_Program/storage_layer/reportDL.py
+++ NaN_Program/storage_layer/reportDL.py
@@ -18,9 +18,10 @@ class ReportDL():
 
     def add_report(self,rep):
         with open(self.csv, 'a', newline='', encoding='utf-8') as csvfile:
-            fieldnames = ["Report-id","Request-id","Employee","Employee-id","Title","Description","Contractor-name","Contractor-id","Status"]
+            fieldnames = ["Report-id","Request-id","Employee","Employee-id","Title","Description","Contractor-name","Contractor-id","Contractor-rating","Status"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({"Report-id":rep.report_id,"Request-id":rep.request_id,"Employee":rep.emp,"Employee-id":rep.emp_id,"Title":rep.title,"Description":rep.descript,"Contractor-name":rep.contract_name,"Contractor-id":rep.contract_id,"Status":rep.status})
+            writer.writerow({"Report-id":rep.report_id,"Request-id":rep.request_id,"Employee":rep.emp,"Employee-id":rep.emp_id,"Title":rep.title,"Description":rep.descript,
+            "Contractor-name":rep.contract_name,"Contractor-id":rep.contract_id,"Contractor-rating":rep.contract_rating,"Status":rep.status})
 
     
     def change_report_info(self,rep_lis):
@@ -29,11 +30,11 @@ class ReportDL():
             header = rep_lis[0]
             writer.writerow(header)
             for dic in rep_lis:
-                writer.writerow([dic["Report-id"],dic["Request-id"],dic["Employee"],dic["Employee-id"],dic["Title"],dic["Description"],dic["Contractor-name"],dic["Contractor-id"],dic["Status"]])
+                writer.writerow([dic["Report-id"],dic["Request-id"],dic["Employee"],dic["Employee-id"],dic["Title"],dic["Description"],dic["Contractor-name"],dic["Contractor-id"],dic["Contractor-rating"],dic["Status"]])
 
 
 if __name__ == "__main__":
     g = ReportDL()
-    t = Report("1","1","Siggi","0","Gluggar","þarf að þrýfa, glugganga","Haraldur","smiður","óklárað")
+    t = Report("1","1","Siggi","0","Gluggar","þarf að þrýfa, glugganga","Haraldur","smiður","7","óklárað")
     g.add_report(t)
     g.change_report_info(g.get_all_reports())
