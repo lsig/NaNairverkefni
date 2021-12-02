d01d43644ea2d971d77c8cdf416c535fe58fa474
Kristófer Fannar Björnsson <kristoferfannar@Kristofers-MacBook-Air.local>
2021-12-02 15:30:37 +0000

    hm

--- NaN_Program/data_files/const.py
+++ NaN_Program/data_files/const.py
@@ -1,7 +1,7 @@
 import os 
-if os.name == 'nt': #til að cleara terminal
+if os.name == 'nt': #til að cleara terminal, hér er os-ið hjá notandanum windows
     CLEAR = 'cls'
-else: # hér hlýtur os.name == 'posix'
+else: # hér hlýtur os.name == 'posix', og os-ið hjá notandanum er mac eða linux.
     CLEAR = 'clear'
 
 STAR = '* '
