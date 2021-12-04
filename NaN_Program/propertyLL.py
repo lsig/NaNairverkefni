9b363abee87a28dc7e9610c6018fde11b798e62c
Maximpop <rudkovpop@gmail.com>
2021-12-04 22:28:58 +0000

    property dic fix

--- NaN_Program/propertyLL.py
+++ NaN_Program/propertyLL.py
@@ -5,16 +5,15 @@ class PropertyLL:
     def __init__(self):
         self.dlapi = DlAPI()
 
-    def add_property(self,prop_lis):
-        if self.is_valid(prop_lis):
-            loc_name = self.replace_loc_num_with_name()
-            prop_lis[0] = loc_name[int(prop_lis[0])-1]['Name']
-            prop = Property(self.assign_id_cont(),prop_lis[0],prop_lis[1],prop_lis[2],prop_lis[3],prop_lis[4],prop_lis[5],prop_lis[6])
+    def add_property(self,prop_dic):
+        if self.is_valid(prop_dic):
+            prop_dic = self.replace_loc_num_with_name(prop_dic)
+            prop = Property(self.assign_id_prop(),prop_dic["Destination"],prop_dic["Address"],prop_dic["Size"],prop_dic["Rooms"],prop_dic["Type"],prop_dic["Property-number"],prop_dic["Extras"])
             self.dlapi.add_property(prop)
             return True
         return False
 
-    def assign_id_cont(self):
+    def assign_id_prop(self):
         all_prop_lis = self.dlapi.get_property_info()
         new_id = int(all_prop_lis[len(all_prop_lis)-1]["id"])+1
         return str(new_id)
@@ -24,28 +23,29 @@ class PropertyLL:
         desti_count = int(all_desti_lis[len(all_desti_lis)-1]["id"])
         return desti_count
 
-    def replace_loc_num_with_name(self):
+    def replace_loc_num_with_name(self,dic):
         loc_names_lis = self.dlapi.get_loc_info()
-        return loc_names_lis
+        dic["Destination"] = loc_names_lis[int(dic["Destination"])-1]['Name']
+        return dic
     
-    def is_valid(self,prop_lis) -> bool:
-        dic = {"destination":int, "address":"both", "size":int, "rooms":int,"type":str,"property-number":"both","extras":str}
+    def is_valid(self,prop_dic) -> bool:
+        dic = {"Destination":int, "Address":"both", "Size":int, "Rooms":int,"Type":str,"Property-number":"both","Extras":str}
         counter = 0
         for key in dic.keys():
             if dic[key] == str and dic[key] != "both":
                 if key.lower() == "extras": #replace empty string with none for extras
-                    if prop_lis[counter] == "":
-                        prop_lis[counter] = "None"
-                get_validation = prop_lis[counter].replace(" ", "").isalpha()
+                    if prop_dic[key] == "":
+                        prop_dic[key] = "None"
+                get_validation = prop_dic[key].replace(" ", "").isalpha()
             elif dic[key] == int and dic[key] != "both":
-                get_validation = prop_lis[counter].replace("-","").isdigit()
+                get_validation = prop_dic[key].replace("-","").isdigit()
             # to check if address or property number are empty    
             if dic[key] == "both":
-                if prop_lis[counter] == "":
+                if prop_dic[key] == "":
                     return False
             #check if Destination is within bounds
             if key.lower() == "destination" and get_validation:
-                if  int(prop_lis[counter]) <= 0 or int(prop_lis[counter]) > self.get_destination_count():
+                if  int(prop_dic[key]) <= 0 or int(prop_dic[key]) > self.get_destination_count():
                     return False
             if get_validation == False:
 
@@ -54,55 +54,48 @@ class PropertyLL:
         return True
 
         
-    def get_all_prop_lis(self):
-        all_prop = self.dlapi.get_property_info()
-        ret_lis = []
-        for row in all_prop:
-            temp_lis = []
-            for keys in row:
-               temp_lis.append(row[keys])
-            ret_lis.append(temp_lis)
-        return ret_lis
     
     def get_all_prop(self):
         all_prop = self.dlapi.get_property_info()
         return all_prop
 
-    def find_prop_id(self,id):
-        ret_lis = []
+
+    def find_prop_id(self,id,all_prop_lis):
         if id.isdigit():
-            all_prop_lis=self.dlapi.get_property_info()
             for dic in all_prop_lis:
                 if int(dic["id"]) == int(id):
-                    for key in dic:
-                        ret_lis.append(dic[key])
-                    return ret_lis 
+                    return dic 
             return None
         return False
 
+    def edit_info(self,edit_prop_dic):
+        if self.is_valid(edit_prop_dic):
+            edit_prop_dic = self.replace_loc_num_with_name(edit_prop_dic)
+            all_lis_prop= self.dlapi.get_property_info()
+            dic = self.find_prop_id(edit_prop_dic["id"],all_lis_prop)
+            con_loc_in_lis = self.find_id_location_prop(dic,all_lis_prop)
+            all_lis_prop[con_loc_in_lis]= edit_prop_dic
+            self.dlapi.change_property_info(all_lis_prop)
+            return True
+        return False
+    
+    def find_id_location_prop(self,dic,all_lis_prop):
+        for i in range(len(all_lis_prop)):
+            if dic == all_lis_prop[i]:
+                return i
 
-
+    def find_prop_by_str(self,user_string,prop_lis,key):
+        ret_lis=[]
+        if user_string.replace(" ",""):
+            for dic in prop_lis:
+                if user_string.lower() in dic[key].lower():
+                    ret_lis.append(dic)
+            return ret_lis
+        return False
 
 if __name__ == "__main__":
     g = PropertyLL()
-    d = g.get_all_prop()
-    print(d)
-    #d = g.find_prop_id('3')
+    #d = g.get_all_prop()
     #print(d)
-#     ret_lis = []
-#     for row in d:
-#         temp_lis = []
-#         for keys in row:
-#             temp_lis.append(row[keys])
-#         ret_lis.append(temp_lis)
-#         temp_lis = []
-#     print(ret_lis[0][1])
-#         #row[keys]
-
-    #print(d[0]['Destination'])
-    #b = g.replace_loc_num_with_name()
-    #print(b[0]['Name'])
-    #print(g.get_destination_count())
-    #g = PropertyLL()
-    #g.add_contractor(["John nohands","Elton john","3549990000","00","Tórshavn"])
-    # g.assign_id_cont()
+    #g.edit_info({"id":"33","Destination":"1", "Address":"Heima 2", "Size":"10", "Rooms":"15","Type":"Best","Property-number":"poom street 2","Extras":"Windows"})
+    print(g.find_prop_by_str("ud",g.get_all_prop(),"Extras"))
