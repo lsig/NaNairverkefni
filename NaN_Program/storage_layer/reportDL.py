865bec24e85f946b5c868433988ff66bc2158cc5
marteinn_vidir <marteinns19@ru.is>
2021-12-06 13:58:09 +0000

    k

--- NaN_Program/storage_layer/reportDL.py
+++ NaN_Program/storage_layer/reportDL.py
@@ -18,10 +18,10 @@ class ReportDL():
 
     def add_report(self,rep):
         with open(self.csv, 'a', newline='', encoding='utf-8') as csvfile:
-            fieldnames = ["Report-id","Request-id","Employee","Employee-id","Title","Description","Contractor-name","Contractor-id","Contractor-rating","Status"]
+            fieldnames = ["Report-id","Request-id","Employee","Employee-id","Title","Description","Contractor-name","Contractor-id","Contractor-rating","Date","Status"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow({"Report-id":rep.report_id,"Request-id":rep.request_id,"Employee":rep.emp,"Employee-id":rep.emp_id,"Title":rep.title,"Description":rep.descript,
-            "Contractor-name":rep.contract_name,"Contractor-id":rep.contract_id,"Contractor-rating":rep.contract_rating,"Status":rep.status})
+            "Contractor-name":rep.contract_name,"Contractor-id":rep.contract_id,"Contractor-rating":rep.contract_rating,"Date":rep.date,"Status":rep.status})
 
     
     def change_report_info(self,rep_lis):
@@ -31,11 +31,11 @@ class ReportDL():
             writer.writerow(header)
             for dic in rep_lis:
                 writer.writerow([dic["Report-id"],dic["Request-id"],dic["Employee"],dic["Employee-id"],
-                dic["Title"],dic["Description"],dic["Contractor-name"],dic["Contractor-id"],dic["Contractor-rating"],dic["Status"]])
+                dic["Title"],dic["Description"],dic["Contractor-name"],dic["Contractor-id"],dic["Contractor-rating"],dic["Date"],dic["Status"]])
 
 
 if __name__ == "__main__":
     g = ReportDL()
-    t = Report("1","1","Siggi","0","Gluggar","þarf að þrýfa, glugganga","Haraldur","smiður","7","óklárað")
-    g.add_report(t)
+    # t = Report("1","1","Siggi","0","Gluggar","þarf að þrýfa, glugganga","Haraldur","smiður","7","óklárað")
+    # g.add_report(t)
     g.change_report_info(g.get_all_reports())
