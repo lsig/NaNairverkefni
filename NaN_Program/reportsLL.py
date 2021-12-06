a8ffe5879402cd0febcd094a06673b3358989cce
Markus Palmason <markuspalmason@Markuss-MacBook-Air.local>
2021-12-06 20:48:57 +0000

    reports are for kings

--- NaN_Program/reportsLL.py
+++ NaN_Program/reportsLL.py
@@ -24,19 +24,35 @@ class ReportsLL:
         all_rep = self.dlapi.get_all_report()
         return all_rep
 
+    def edit_report_info(self, edit_rep_dic):
+        # NOTETOSELF:yfirmaður þarf að geta samþykkt viðhaldsskýrslur, og starfsmenn þurfa að geta séð hvaða skýrslur, sem þeir eiga, eru samþykktar og hverjar ekki.
+        # Ef skýrsla er ekki samþykkt, þarf starfsmaður að geta breytt upplýsingum í skýrslunni.
+        if self.report_validation(edit_rep_dic):
+            all_list_rep = self.dlapi.get_all_report()
+
+        # NOTETOSELF: Skýrslur merktar "pending", "accepted", eða "rejected"
+        # yfirmaður getur merkt skýrslu accepted eða rejected, en starfsmaður getur merkt skýrslu pending.
+        # # # 
 
     def report_validation(self, rep_dic):
+        # a dictionairy for title, description, contractor-name and contractor-id.
         dic = {"Title":str, "Description":"both", "Contractor-name":str, "Contractor-id":int}
         counter = 0
         for key in dic.keys():
+            # checking if the dictionairy key is a string and not both string and integer, and if so, remove spaces.
             if dic[key] == str and dic[key] != "both":
                 get_validation = rep_dic[key].replace(" ", "").isalpha()
+            # checking if the dictionairy key is an integer and not both string and integer, and if so, check if integer is in fact a number/digit.
             elif dic[key] == int and dic[key] != "both":
                 get_validation = rep_dic[key].isdigit()
+            # checking if the dictionairy key is both a string and integer, and if so, check if the key is empty. If so, the program will return False.
             if dic[key] == "both":
                 if rep_dic[key] == "":
                     return False
 
+if __name__ == "__main__":
+    r = ReportsLL()
+    r.add_report
 
         
          
