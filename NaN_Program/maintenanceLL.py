5d9d8a7f0a72e288f7bee39e15b3172368444903
marteinn_vidir <marteinns19@ru.is>
2021-12-07 21:20:46 +0000

    baby

--- NaN_Program/maintenanceLL.py
+++ NaN_Program/maintenanceLL.py
@@ -1,5 +1,7 @@
 from models.maintenance import Maintenance
 from storage_layer.DLAPI import DlAPI
+from EmployeeLL import EmployeeLL
+from propertyLL import PropertyLL
 from datetime import date, datetime
 
 
@@ -7,21 +9,30 @@ class MaintenanceLL:
 
     def __init__(self):
         self.dlapi = DlAPI()
+        self.empLL = EmployeeLL()
+        self.propLL = PropertyLL()
         
 
 
     def add_maintenance(self,main_dic,boss_id):
         # id,Date-from,Date-to,Frequency,Employee,Employee-id,Title,Description,
         # Location,Property,Property-number,Property-id,Priority,Suggested-contractor,Status
+        
         # dic_fromat = {"Date-to(dd-mm-yyyy)"":int,"Frequency":int,"Employee-id":int,"Title":str,"Description":"both","Property-id":int,"Priority":int,"Suggested-contractor":str}
+        boss_loc = 
         curr_date = datetime.date(datetime.now())
-        emp_name =None
-        boss_loc = None
-        prop,prop_nr = None,None
-        priority = None
-        main_job = Maintenance(self.assign_id(),curr_date,main_dic["Date-to(dd-mm-yyyy)"],main_dic["Frequency:Week(1) or Month(2)"],emp_name,main_dic["Employee-id"],main_dic["Title"],
-        main_dic["Description"],boss_loc,prop,prop_nr,main_dic["Property-id"],priority,main_dic["Suggested-contractor"],0)
-        self.dlapi.add_maintenance_job(main_job)
+        valid_bool,key = self.is_valid(curr_date,main_dic)
+        if valid_bool:
+            
+            emp_name =None
+            boss_loc = None
+            prop,prop_nr = None,None
+            priority = None
+            main_job = Maintenance(self.assign_id(),curr_date,main_dic["Date-to(dd-mm-yyyy)"],main_dic["Frequency:Week(1) or Month(2)"],emp_name,main_dic["Employee-id"],main_dic["Title"],
+            main_dic["Description"],boss_loc,prop,prop_nr,main_dic["Property-id"],priority,main_dic["Suggested-contractor"],0)
+            self.dlapi.add_maintenance_job(main_job)
+            return True,key
+        return False,key
 
 
     def assign_id(self):
@@ -32,50 +43,71 @@ class MaintenanceLL:
         return str(1)
 
     def is_valid(self,today,main_dic):
-        dic = {"Date-to(dd-mm-yyyy)":int,"Frequency":int,"Employee-id":int,"Title":str,"Description":"both","Property-id":int,"Priority":int,"Suggested-contractor":str}
+        dic = {"Date-to(dd-mm-yyyy)":int,"Frequency:Week(1) or Month(2)":int,"Employee-id":int,"Title":str,"Description":"both","Property-id":int,"Priority":int,"Suggested-contractor":str}
         for key in dic.keys():
             if dic[key] == str:
                 get_validation = main_dic[key].replace(" ", "").isalpha()
             elif dic[key] == int:
-                get_validation = main_dic[key].replace("-","").isdigit()
-            # to check if the phone number is a valid length    
+                print(main_dic[key])
+                get_validation = main_dic[key].replace("-","").isdigit()  
+            elif dic[key] == "both" and main_dic[key] == "":
+                return False,key
+            else:
+                get_validation = True
 
-            if key == "Date-to(dd-mm-yyyy)" and get_validation:
+            if key == "Date-to(dd-mm-yyyy)" and get_validation and main_dic[key] != "":
                 if len(main_dic[key]) != 10:
                     return False, key
-                date_time = self.check_date(main_dic[key].split())
+
+                date_time = self.check_date(main_dic[key].split("-"))
                 if date_time:
                     pass
                 else:
                     return False,key
-            if key == "Frequency":
+            elif key == "Date-to(dd-mm-yyyy)" and main_dic[key] != "":
+                get_validation = True
+
+            if key == "Frequency:Week(1) or Month(2)":
                 if int(main_dic[key]) == 1 or int(main_dic[key]) == 2:
                     pass
                 else:
                     return False,key
-                
+            if key == "Employee-id" and get_validation:
+                if self.find_emp(main_dic[key]) == {}:
+                    return False,key
+
+            if key == "Property-id" and get_validation:
+                if self.get_property(main_dic[key]) == {}:
+                    return False,key
+            if key == "Suggested-contractor" and main_dic[key] == "":
+                get_validation = True
                 
             if get_validation == False:
-
                     return False, key
-        if main_dic["Frequency"] == "1":
+        if main_dic["Frequency:Week(1) or Month(2)"] == "1":
             freq = 7
         else:
             freq = 30
         if today >= date_time:
             if (date_time-today).days() <= freq:
                 return False,"Date-to(dd-mm-yyyy)","Frequency"
-        return True 
+        return True,"" 
 
 
     def check_date(self,date):
-        if date[0] == 2 and date[1] == 2 and date[2] == 4:
-            if date[0] > 0 and date[0] < 32 and date[1] > 0 and date[1] < 13:
+        if len(date[0]) == 2 and len(date[1]) == 2 and len(date[2]) == 4:
+            if int(date[0]) > 0 and int(date[0]) < 32 and int(date[1]) > 0 and int(date[1]) < 13:
                 date_time=datetime(int(date[2]),int(date[1]),int(date[0])).date()
                 if date_time > datetime.date(datetime.now()):
                     return date_time
         return False
 
+
+    def find_emp(self,id):
+        return self.empLL.find_emp_id(id,self.dlapi.get_all_emp())
+
+    def get_property(self,id):
+        return self.propLL.find_prop_id(id,self.dlapi.get_property_info())
 
     
 
@@ -91,8 +123,10 @@ if __name__ == "__main__":
     print((x1- x).days)
     # print(x.strftime("%B"))
 
-    dic_fromat = {"Date-to(dd-mm-yyyy)":"20-12-2020","Frequency(number of days)":"7","Employee-id":"2","Title":"hani","Description":"hehe","Property-id":"3","Priority":"1","Suggested-contractor":" "}
-    # g = MaintenanceLL()
-    # g.add_maintenance(dic_fromat,1)
+    dic_fromat = {"Date-to(dd-mm-yyyy)":"20-12-2022","Frequency:Week(1) or Month(2)":"1","Employee-id":"2","Title":"hani","Description":"hehe","Property-id":"3","Priority":"1","Suggested-contractor":""}
+    g = MaintenanceLL()
+    # print(dic_fromat[])
+    print(g.add_maintenance(dic_fromat,1))
+
     if x:
         print("yeah")
