eff4ece7f4ffa0ec8599e82acc697da6ef9bac20
Maximpop <rudkovpop@gmail.com>
2021-12-07 21:38:47 +0000

    jobLL

--- NaN_Program/jobLL.py
+++ NaN_Program/jobLL.py
@@ -1,19 +1,21 @@
 from storage_layer.DLAPI import DlAPI
 from models.job import Job
 from datetime import datetime
+from EmployeeLL import EmployeeLL
 #change job will require self.boss fetching!!!!!!!!!!!!!!!!!!!!! note to self
 class JobLL:
     def __init__(self):
         self.dlapi = DlAPI()
+        self.empLL = EmployeeLL()
         self.boss_loc = ""
     
     def add_job(self,job_dic,id):
-        self.boss_loc = self.get_emp_location(id)
+        self.boss_loc = self.empLL.get_emp_location(id)
         if self.is_valid(job_dic):
             
             auto_id = self.assign_id_job()
             cur_date = datetime.date(datetime.now())
-            emp_name = self.find_employee_name(job_dic["Employee-id"])
+            emp_name = self.empLL.find_employee_name(job_dic["Employee-id"])
             #con_name = self.get_con_name_and_location(job_dic["Suggested-contractor"])["Name"]
             job = Job(auto_id,cur_date,emp_name,job_dic["Employee-id"],job_dic["Title"],job_dic["Description"],self.boss_loc,self.prop_address_from_id(job_dic["Property-id"])[0],self.prop_address_from_id(job_dic["Property-id"])[1],job_dic["Property-id"],job_dic["Priority"],job_dic["Suggested-contractor"],"0")
             self.dlapi.add_job(job)
@@ -28,21 +30,21 @@ class JobLL:
             new_id = int(all_job_lis[len(all_job_lis)-1]["id"])+1
         return str(new_id)
     
-    def find_employee_name(self,id):
-        employee_names = self.dlapi.get_all_emp()
-        for dic in employee_names:
-            if int(id) == int(dic["id"]):
-                emp_name = dic["Name"]
-        return emp_name
+    # def find_employee_name(self,id):
+    #     employee_names = self.dlapi.get_all_emp()
+    #     for dic in employee_names:
+    #         if int(id) == int(dic["id"]):
+    #             emp_name = dic["Name"]
+    #     return emp_name
 
-    def get_emp_location(self,id):
-        emp_lis = self.dlapi.get_all_emp()
-        for dic in emp_lis:
-            if int(id) == int(dic["id"]):
-                boss_location = dic["Destination"]
-                return boss_location
-        none_val = "None"
-        return none_val
+    # def get_emp_location(self,id):
+    #     emp_lis = self.dlapi.get_all_emp()
+    #     for dic in emp_lis:
+    #         if int(id) == int(dic["id"]):
+    #             boss_location = dic["Destination"]
+    #             return boss_location
+    #     none_val = "None"
+    #     return none_val
         
         #big validation check
     def is_valid(self,job_dic) -> bool:
@@ -53,7 +55,7 @@ class JobLL:
             elif dic[key] == int and dic[key] != "both":
                 get_validation = job_dic[key].replace("-","").isdigit()
                 if key == "Employee-id":
-                    if self.boss_loc != self.get_emp_location(job_dic["Employee-id"]):
+                    if self.boss_loc != self.empLL.get_emp_location(job_dic["Employee-id"]):
                         return False
                 if key == "Property-id":
                     if self.prop_address_from_id(job_dic["Property-id"])[2] != self.boss_loc:
@@ -119,7 +121,7 @@ class JobLL:
         
 
     def edit_info(self,edit_job_dic,id): #virkar ekki find id location virkar ekki
-        self.boss_loc = self.get_emp_location(id)
+        self.boss_loc = self.empLL.get_emp_location(id)
         if self.is_valid(edit_job_dic):
             all_lis_job = self.dlapi.get_jobs()
             dic = self.find_job_id(edit_job_dic["id"],all_lis_job)
@@ -148,11 +150,11 @@ class JobLL:
 if __name__ == "__main__":
     g = JobLL()
     #print(g.find_employee_name("5"))
-    #g.add_job({"Employee-id":"2","Title":"Maxim","Description":"something","Property-id":"1","Priority":"1","Suggested-contractor":"1"},"1")
+    g.add_job({"Employee-id":"2","Title":"Maxim","Description":"something","Property-id":"1","Priority":"1","Suggested-contractor":"1"},"1")
     #bool2 = g.is_valid({"Employee-id":"2","Title":"something1","Description":"Do something","Property-id":"1","Priority":"1","Suggested-contractors":"1"})
     #print(bool2)
     #print(g.prop_address_from_id("1"))
     #print(g.find_jobs_by_str("o",g.get_all_jobs(),"Title"))
     #print(g.get_con_name_and_location("1")["Name"])
     #g.edit_info({"id":"1","Date-created":"2021-12-05","Employee":"Jan Jacobsen","Employee-id":"1","Title":"small window clean","Description":"cleandd the windows!","Location":"Longyearbyen","Property":"Vei 217","Property-number":"F959594","Property-id":"1","Priority":"1","Suggested-contractor":"1","Status":"0"},"1")
-    print(g.get_all_jobs())
+    #print(g.get_all_jobs())
