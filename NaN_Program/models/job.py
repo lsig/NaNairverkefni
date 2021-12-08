69003f57d6f73bb03dad7aa7a235d5a98d40d776
Maximpop <rudkovpop@gmail.com>
2021-12-08 16:06:20 +0000

    JobLL con name and id

--- NaN_Program/models/job.py
+++ NaN_Program/models/job.py
@@ -1,5 +1,5 @@
 class Job:
-    def __init__(self,id,date_created,employee,employee_id,title,description ,location,propertyName,propertyNumber,propertyid,priority,suggested_contractors,status):
+    def __init__(self,id,date_created,employee,employee_id,title,description ,location,propertyName,propertyNumber,propertyid,priority,suggested_contractors_id,suggested_contractors,status):
         self.id = id 
         self.date_cr = date_created
         self.empl = employee
@@ -11,6 +11,7 @@ class Job:
         self.prop_num = propertyNumber
         self.prop_id = propertyid
         self.prior = priority
+        self.sug_con_id = suggested_contractors_id
         self.sug_con = suggested_contractors
         self.stat = status
 
