27c33ae3f0af4c7490afdfab137ce0f254c58fae
Maximpop <rudkovpop@gmail.com>
2021-12-09 15:59:13 +0000

    Contractor validation fix

--- NaN_Program/data_files/const.py
+++ NaN_Program/data_files/const.py
@@ -15,7 +15,7 @@ CONTACTTEMPLATE = ['Name', 'Social Security', 'Address', 'Phone', 'GSM', 'Destin
 CONTRACTTEMPLATE = ["Employee-id","Title","Description","Property-id","Priority(ASAP; Now; Emergency)","Suggested-contractor(id)"] #listi fyrir verkbeiðnir
 REGCONTRACTTEMPLATE = CONTRACTTEMPLATE + ["Frequency(Week: 1, or Month: 2)", '"Date-to(dd-mm-yyyy)"']
 PROPERTYTEMPLATE = ['Destination', 'Address', 'Size', 'Rooms', 'Type', 'Property-number', 'Extras'] # listi fyrir fasteignir
-CONTRACTORTEMPLATE = ["Name","Contact-name","Profession","Phone","Working-hours","Location"] #listi fyrir verktaka
+CONTRACTORTEMPLATE = ["Name","Contact-name","Profession","Phone","Working-hours"] #listi fyrir verktaka, location kemur frá boss
 DESTINATIONTEMPLATE = ['Name', 'Country' ,'Airport', 'Phone', 'Working-hours', 'Manager', 'Social Security', 'Address', 'Phone-manager', 'GSM']
 REPORTTEMPLATE = ['Report-id', 'Request-id', 'Employee', 'Employee-id', 'Title', 'Description', 'Location', 'Property', 'Property-number', 'Property-id', 'Contractor-name', 'Contractor-id', 'Contractor-rating', 'Date', 'Commission', 'Status']
 
