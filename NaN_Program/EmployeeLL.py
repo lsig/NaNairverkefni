1ba78b38306b92cba8294acee6cc4d7ebb8a9154
Lsig <logis21@ru.is>
2021-12-09 14:11:21 +0000

    ble

--- NaN_Program/EmployeeLL.py
+++ NaN_Program/EmployeeLL.py
@@ -111,7 +111,7 @@ class EmployeeLL:
 
             # to check if address or property number are empty    
             if dic[key] == "both":
-                if emp_dic[key] == "":
+                if emp_dic[key] == "" or (len(emp_dic[key]) > 25):
                     return False, key
             #check if Destination is within bounds
             if key.lower() == "destination" and get_validation:
@@ -120,7 +120,7 @@ class EmployeeLL:
                         loc_correct = True
                 if loc_correct == False:
                     return False, key
-            if key.lower() == "phone" and get_validation:
+            if (key.lower() == "phone" or key.lower() == "gsm") and get_validation:
                 if 7 > len(emp_dic[key]) or len(emp_dic[key]) > 15:
                     return False, key
         
