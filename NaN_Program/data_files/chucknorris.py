b44a9731752fbb9257563e00d4ac2e962912fe22
Markus Palmason <markuspalmason@Markuss-MacBook-Air.local>
2021-12-09 23:08:45 +0000

    kristo er bestur i chuck norris jokes

---
+++ NaN_Program/data_files/chucknorris.py
@@ -0,0 +1,17 @@
+CHUCKNORRIS = [
+    'The dinosaurs looked at Chuck Norris the wrong way once. you know what happened to them.',
+    'Chuck Norris does not sleep, he waits',
+    'Some kids pee their name in the snow. Chuck Norris can pee his name into concrete.',
+    'Champions are the breakfast of Chuck Norris.',
+    'Death once had a near-Chuck-Norris experience.',
+    'Chuck Norris once had a heart attack. His heart lost.',
+    'Chuck Norris beat the sun in a staring contest.',
+    'Freddy Krueger has nightmares about Chuck Norris.',
+    'Chuck Norris can clap with one hand.',
+    'We live in an expanding universe. All of it is trying to get away from Chuck Norris.',
+    'Chuck Norris doesn’t breathe, he holds air hostage.',
+    'It takes Chuck Norris 20 minutes to watch 60 Minutes.',
+    'Chuck Norris is the reason why Waldo is hiding.',
+    'Chuck Norris can tie his shoes with his feet.',
+    'Chuck Norris can build a snowman out of rain.'
+    ]
