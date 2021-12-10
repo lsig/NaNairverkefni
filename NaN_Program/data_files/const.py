1436fd310d7b21ebe40232754493b4356b800c8d
Lsig <logis21@ru.is>
2021-12-10 23:07:47 +0000

    le

--- NaN_Program/data_files/const.py
+++ NaN_Program/data_files/const.py
@@ -7,9 +7,11 @@ else: # hér hlýtur os.name == 'posix', og os-ið hjá notandanum er mac eða l
 SLASH = os.sep
 STAR = '* '
 DASH = '-'
-SLEEPTIME = 1 #var 1.5
+SLEEPTIME = 1 
 INVALID = 'Invalid option, try again!'
 QUIT = 'Q'
+
+#Templates for working with different aspects of the program
 
 CONTACTTEMPLATE = ['Name', 'Social Security', 'Address', 'Phone', 'GSM', 'Destination'] #listi fyrir starfsmenn
 CONTRACTTEMPLATE = ["Employee-id", "Title", "Description", "Property-id", "Priority(ASAP; Now; Emergency)", "Suggested-contractor(id)"] #listi fyrir verkbeiðnir
@@ -23,14 +25,3 @@ CREATEREPORTTEMPLATE = ['Description', 'Contractor-id', 'Commission','Total-cost
 
 JOBDICT = {'id': 5, 'Date-created': 15, 'Employee': 20, 'Title': 25, 'Location': 20, 'Property': 25, 'Priority(ASAP; Now; Emergency)': 12, 'Suggested-contractors': 30, 'Status':10,  'Type': 15}
 REPORTDICT = {'Report-id': 5, 'Employee': 20, 'Title': 25, 'Location': 20, 'Property': 25, 'Contractor-rating': 10, 'Date': 15, 'Commission': 15, 'Status': 10}
-
-
-
-# CONTRACTORTEMPLATE = ['Nafn verktaka', 'Nafn tengiliðs','Nafn sérhæfingu', 'Sími', 'Opnunartími','Áfangastaður'] #listi fyrir verktaka
-#PROPERTYTEMPLATE = ['Áfangastaður', 'Heimilisfang', 'Stærð', 'Herbergi', 'Tegund', 'Númer fasteignar', 'Auka'] # listi fyrir fasteignir
-#CONTRACTTEMPLATE = ['Stofnun verkbeiðnar','Starfsmaður', 'Starfsmanna ID', 'Titill', 'Lýsing', 'Áfangastaður', 'Fasteign', 'Númer fasteignar', 'Fasteignar ID', 'Forgangur','Staða'] #listi fyrir verkbeiðnir
-# CONTACTTEMPLATE = ['Nafn', 'Kennitala', 'Heimilisfang', 'Heimasími', 'GSM símanúmer', 'Netfang', 'Áfangastaður', 'Yfirmaður'] #listi fyrir starfsmenn
-
-if __name__ == '__main__':
-    print( sum(JOBDICT.values()) )
-    print( sum(REPORTDICT.values()) )
